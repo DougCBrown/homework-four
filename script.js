1f4ee44963c97744b4e1c793ab3cde1837d579fa
DougCBrown <98bluecamaro@gmail.com>
2020-07-30 14:14:19 -0400

    working on the JavaScript code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-const questionsElement = document.getElementById('question')
+const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-const shuffledQuestions, currentQuestionIndex
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
 
@@ -16,22 +18,57 @@ function startGame() {
 }
 
 function setNextQuestion() {
-    showQuestions(suffledQuestions[currentQuestionsIndes])
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 } 
 
-function showQuestions(question) {
-    questionsElement.innerText = question.question
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.textbutton.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+function selectAnswer(e) {
+    const selectButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)  
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })  
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    }   else {
+        element.classList.add('wrong')
+    }    
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
     {
-        questions: 'what is 2 + 2',
-        answere: [
+        question: 'what is 2 + 2?',
+        answers: [
             { text: '4', correct: true},
             { text: '22', correct: false}
         ]
